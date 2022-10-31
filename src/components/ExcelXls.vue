72bedd332f07622b4f88c0c17c961a07fbfefd76
maclanelf <maclanelf@qq.com>
2022-10-31 10:25:36 +0800

    init my project

---
+++ src/components/ExcelXls.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>
+        <el-upload accept=".xlsx,.xls" :before-upload="upload" action="" :limit="1">
+            <template #trigger>
+                <el-button type="danger" round>select file</el-button>
+                <div class="el-upload__tip text-red bottomRow">
+                    {{startRow}} / {{maxRow}}
+                </div>
+            </template>
+            <!-- <template #tip>
+                <div class="el-upload__tip text-red">
+                    {{startRow}} / {{maxRow}}
+                </div>
+            </template> -->
+        </el-upload>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ExcelXls',
+    components: {},
+    props: [''],
+    data() {
+        return {
+
+        };
+    }
+}
+</script>
+<script setup>
+import { ref, onMounted, defineExpose } from "vue";
+import { UploadFilled, Edit, Upload } from "@element-plus/icons-vue"
+import { ElMessage } from 'element-plus'
+import { read, readFile, utils, writeFileXLSX } from "xlsx";
+
+const worksheet = ref(null);
+const startRow = ref(2)
+const maxRow = ref(0)
+
+//上传文件并将excel的worksheet对象进行存储,以便取值
+const upload = (file) => {
+    const fileReader = new FileReader()
+    fileReader.onload = (ev) => {
+        try {
+            const data = ev.target.result
+            const workbook = read(data, {
+                type: "binary"
+            })
+            // 取第一张表
+            const wsname = workbook.SheetNames[0]
+            // 生成json表格内容
+            worksheet.value = workbook.Sheets[wsname]
+            const ws = utils.sheet_to_json(workbook.Sheets[wsname])
+            // console.log(worksheet.value["B4"]);
+            //获取excel单表的最大有效值范围,A1:H457
+            const valid = worksheet.value["!ref"]
+            const valid1 = valid.substring(valid.indexOf(":")+1)
+            maxRow.value = /\d+/g.exec(valid1)[0]
+        } catch (e) {
+            return false
+        }
+    }
+    fileReader.readAsBinaryString(file)
+    // 阻止ElUpload默认上传
+    return false
+}
+//获取单元格的值
+const getValue = (address) => {
+    debugger // eslint-disable-line
+    if (worksheet.value === null || worksheet.value === undefined) {
+        ElMessage({
+            message: '没检测到文件,要不上传个文件试试?',
+            type: 'warning',
+        })
+        return 
+    }
+    if (worksheet.value[address] === null || worksheet.value[address] === undefined) {
+        ElMessage({
+            message: '未找到单元格 '+address+' 的值,请确认是否填写正确',
+            type: 'warning',
+        })
+    }
+    return worksheet.value[address]
+}
+//setup标签中需要暴露出去,父组件才能调用,setup()中不用
+defineExpose({ getValue ,startRow,maxRow})
+</script>
+<style scoped>
+.bottomRow{
+    margin-left: 10px;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,348 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <el-input v-model="insert" :rows="3" type="textarea" @input="changeVal" placeholder="请输入sql, 如insert into COMPANIES (COMPANY, TYPE, NAME, ADDRESS1) values ('HL00R', 'V', '南京汇润霖信息科技有限公司(测试用）', '南京市秦淮区太平南路465号');" />
+        <el-container>
+            <el-main class="main">
+                <div v-for="(item,index) in input1Arr" :key="index">
+                    <el-input :name="Object.keys(item)[0]" class="w-50 m-2 main_input" :placeholder="`${index+1}.${Object.keys(item)[0]}`" disabled>
+                    </el-input>
+                    <el-input v-model="item[Object.keys(item)[0]]" class="w-50 m-2 main_input" :placeholder="Object.values(item)[0]" @input="changeExcel(item)">
+                        <!-- <template #append>
+                            <el-button :icon="Check" type="success" plain round/>
+                        </template> -->
+                        <template #suffix>
+                            <el-icon class="el-input__icon" :size="20" :color="item.color=='red'?'red':'grey'" @click="toExcel(item)">
+                                <EditPen />
+                            </el-icon>
+                        </template>
+                    </el-input>
+                </div>
+            </el-main>
+        </el-container>
+        <el-button type="primary" round @click="generateSql()">生成sql</el-button>
+        <el-button type="danger" round @click="generateSqlBatch">批量生成sql</el-button>
+        <component :is="ExcelXls" ref="Excel" class="ExcelXls"></component>
+
+        <!-- 抽屉 -->
+        <el-drawer v-model="drawer2" :direction="direction" size='80%' custom-class="myDrawer">
+            <template #title>
+                <div style="height:450px;overflow-y: scroll;">
+                    <h4 v-for="(item,index) in sqlBatch" :key="index">
+                        {{item}}
+                    </h4>
+                </div>
+            </template>
+            <template #footer>
+                <div style="flex: auto">
+                    <el-button @click="cancelClick">cancel</el-button>
+                    <el-button type="primary" @click="confirmClick">confirm</el-button>
+                </div>
+            </template>
+        </el-drawer>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    name: 'HelloWorld',
+    data() {
+        return {
+        };
+    },
+    methods: {},
+    mounted() { },
+}
+</script>
+<script setup>
+import { ref, computed } from 'vue'
+import { nextTick } from 'process'
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { Check, EditPen } from "@element-plus/icons-vue"
+import ExcelXls from './ExcelXls.vue'
+
+
+let input1Arr = ref([{ Field1: 'Value1' }, { Field2: 'Value2' }, { Field3: 'Value3' }])
+let input2Arr = ref([{ Field1: 'Value1' }, { Field2: 'Value2' }, { Field3: 'Value3' }])
+const insert = ref('')
+const sql = ref('')
+const sqlBatch = ref([])
+
+const drawer2 = ref(false)
+const direction = ref('rtl')
+
+const Excel = ref(null)
+
+//检测输入sql,并拆分处理
+function changeVal() {
+    const reg = /[) values (|)values (|) values(|)values(]/g
+    if (reg.test(insert.value)) {
+        //获取属性名字
+        let field = insert.value.substring(insert.value.indexOf('(') + 1, insert.value.indexOf(')'))
+        // input1Arr.value = field.split(',')
+
+        //获取属性对应的值
+        let sub1 = insert.value.substring(insert.value.indexOf('values') + 6)
+        let value = sub1.substring(sub1.indexOf('(') + 1, sub1.lastIndexOf(')'))
+        //如果某个value内包含了多个,做一个简单的转换,---暂时不做
+        /* let regc = /',{1,n}'|'，{1,n}'/g
+        const comma = regc.exec(value)
+        if (comma!==null) {
+            comma.forEach((item,index) => {
+                if (!/to_date|to_char/g.test(item)){
+                    regc.lastIndex = 0
+                    value = value.replace(item,item.replaceAll(regc,'_'))
+                }
+            })
+        } */
+        if (/',{2,n}'|'，{2,n}'/g.test(value)) {
+            value = value.replaceAll(/',{2,n}'|'，{2,n}'/g,'_')
+        }
+        //split后的值可能会包含to_date('06-10-2022 20:50:26', 'dd-mm-yyyy hh24:mi:ss')这样的值被误拆分,需要处理
+        let valueArr = value.split(',')
+        let valueArr1 = []
+        valueArr.forEach((item, index) => {
+            // if(index==17) debugger // eslint-disable-line
+            const regx = /to_date|to_char/g
+            if (regx.test(item)) {
+                valueArr1.push(item.trim() + ',' + valueArr[index + 1].trim())
+            } else if (index !== 0 && regx.test(valueArr[index - 1])) {
+                return
+            } else {
+                valueArr1.push(item.trim())
+            }
+        })
+
+        //对field和value做键值对匹配
+        let fieldArr = field.split(',')
+        input1Arr.value = fieldArr.map((item, index) => {
+            let obj = {}
+            obj[item] = valueArr1[index]
+            return obj
+        })
+    }
+}
+//批量生成sql
+function generateSqlBatch() {
+    // debugger // eslint-disable-line
+    //先对从哪行开始计数做一个粗略计算
+    const first = input1Arr.value.find((item, index) => {
+        return !!item.isExcel
+    })
+    if (first !== undefined) {
+        let reg = /\+.*\+/g
+        let reg1 = /\d+/g
+        const v1 = reg.exec(Object.values(first)[0])
+        const v2 = v1 === null ? reg1.exec(Object.values(first)[0])[0] : reg1.exec(v1[0])[0]
+        ElMessageBox.confirm(
+            '检测到您是想从第' + v2 + '行开始',
+            'Warning',
+            {
+                confirmButtonText: '是',
+                cancelButtonText: '否',
+                type: 'warning',
+            }
+        )
+            .then(() => {
+                Excel.value.startRow = v2
+                try {
+                    generateSqlBatchs()
+                } catch (e) {
+                    ElMessage({
+                        message: e,
+                        type: 'warning',
+                    })
+                }
+            })
+            .catch(() => {
+                ElMessage({
+                    message: '暂时未开发~~~~,',
+                    type: 'warning',
+                })
+            })
+    } else {
+        ElMessage({
+            type: 'warning',
+            message: '未指定取值模式,无法批量生成sql',
+        })
+    }
+    
+}
+//批量生成sql核心方法
+function generateSqlBatchs(){
+    // debugger // eslint-disable-line
+    //先对input1Arr进行copy ,目的是为防止回写值到页面
+    //input1Arr具有响应式,按如下处理,得到失去响应式的对象
+    const origin= input1Arr.value.map((item) => {
+        let obj = {}
+        Object.keys(item).forEach((ele,inx) => {
+            obj[ele] = item[ele]
+        })
+        return obj 
+    })
+    //对input2Arr重新写值并循环调用generateSql
+    const startRow = Excel.value.startRow
+    const maxRow = Excel.value.maxRow
+    if (maxRow==0) ElMessage({ type: 'warning', message: '没检测到文件,要不上传个文件试试?', })
+    for (let i = startRow; i < parseInt(maxRow); i++) {
+        const ina = origin.map((item, index) => {
+            // debugger // eslint-disable-line
+            if (item.isExcel) {
+                let reg = /\+.*\+/g
+                let reg1 = /\d+/g
+                const v1 = reg.exec(Object.values(item)[0])
+                const ev = v1 === null ? reg1.exec(Object.values(item)[0]) : reg1.exec(v1[0])
+                if (ev === null) {
+                    ElMessage({ type: 'warning', message: Object.values(item)[0]+' 值有误', })
+                    return item
+                } else {
+                    let vnumber = i == startRow ? parseInt(ev[0]) : parseInt(ev[0]) + 1
+                    reg.lastIndex = 0
+                    reg1.lastIndex = 0
+                    item[Object.keys(item)[0]] = v1===null ? Object.values(item)[0].replace(ev[0],vnumber) : Object.values(item)[0].replace(v1[0],v1[0].replace(ev[0],vnumber))
+                    // console.log(item,'item');
+                    return item
+                }
+                
+            } else {
+                return item
+            } 
+        })
+        //执行generateSql
+        generateSql(ina)
+    }
+}
+//生成最终sql
+function generateSql(ina) {
+    //获取主表名称
+    let ownertable = insert.value.substring(insert.value.indexOf("insert into") + 11, insert.value.indexOf('('))
+    let sql_Field = ''
+    let sql_Value = ''
+    ina = ina!==undefined ? ref(ina) : input1Arr
+    ina.value.forEach((item, index) => {
+        const reg = /\+.*\+|~.*~/g
+        sql_Field += index != ina.value.length - 1 ? Object.keys(item)[0] + ',' : Object.keys(item)[0]
+        //处理了如何获取单元格的值
+        if (!!item.isExcel) {
+            const matchArr = Object.values(item)[0].match(reg)
+            if (matchArr === null) {
+                const value = Excel.value.getValue(Object.values(item)[0]).v
+                sql_Value += index != ina.value.length - 1 ? value + ',' : value
+            } else {
+                let realvalue
+                matchArr.forEach((match, index) => {
+                    // debugger // eslint-disable-line
+                    const replaceValue = match.replaceAll(/\+|~/g, "").trim()
+                    const excelValue = Excel.value.getValue(replaceValue).v
+                    realvalue = Object.values(item)[0].replace(replaceValue, excelValue)
+                })
+
+                const flag = reg.test(realvalue)
+                reg.lastIndex = 0 //test或者exec执行完成后lastIndex会变,需要重置为0,下一次执行其他函数才会匹配到结果
+                const realreg = /\+.*\+/g.exec(realvalue)
+                const flag1 = realreg !==null ? realreg.index ==0 : false
+                // const flag1 = /\+.*\+/g.exec(realvalue).index == 0
+                const vflag = flag && flag1
+                // debugger // eslint-disable-line
+                switch (flag1) {
+                    case true:
+                        realvalue = realvalue.replaceAll(/\+/g, "").trim()
+                        break;
+                    case false:
+                        if (/~.*~/g.test(realvalue)) {
+                            realvalue = realvalue.replaceAll(/~/g, "").trim()
+                        } else {
+                            realvalue = realvalue.replaceAll(/\+/g, "'").trim()
+                        }
+                        break;
+                    default:
+                        realvalue = realvalue.replaceAll(/\+|~/g, "").trim()
+                }
+
+                switch (vflag) {
+                    case true:
+                        sql_Value += index != ina.value.length - 1 ? "'" + realvalue + "'" + ',' : "'" + realvalue + "'"
+                        break;
+                    case false:
+                        sql_Value += index != ina.value.length - 1 ? realvalue + ',' : realvalue
+                        break;
+                    default:
+                        sql_Value += index != ina.value.length - 1 ? "'" + realvalue + "'" + ',' : "'" + realvalue + "'"
+                }
+            }
+        } else {
+            sql_Value += index != ina.value.length - 1 ? Object.values(item)[0] + ',' : Object.values(item)[0]
+        }
+    })
+    sql_Field = ' (' + sql_Field + ') '
+    sql_Value = ' (' + sql_Value + ') '
+    sql.value = "insert into " + ownertable.trim() + sql_Field + ' values ' + sql_Value + ';'
+    // console.log(sql);
+    sqlBatch.value.push(sql.value)
+    drawer2.value = true
+}
+//将键值对进行标记,需要获取excel中对应的值则勾选
+function toExcel(item) {
+    // debugger // eslint-disable-line
+    item.color = item.color == 'red' ? 'grey' : 'red'
+    /* input1Arr.value.forEach((ele, inx) => {
+        if (Object.keys(ele)[0] == Object.keys(item)[0]) {
+            if (item.color == 'red') {
+                input1Arr.value[inx].isExcel = true
+            }
+            if (item.color == 'grey') {
+                input1Arr.value[inx].isExcel = false
+            }
+        }
+    }) */
+    item.isExcel = item.color == 'red' ? true :false
+    // console.log(input1Arr.value);
+    // input1Arr.value = [...input1Arr.value,{...item,isExcel:true}]
+}
+//自动标记是否是isExcel
+function changeExcel(item){
+    let reg = /\+.*\+|~.*~/g
+    if (reg.test(Object.values(item)[0])) {
+        item.color = 'red'
+        item.isExcel = true
+    } else {
+        item.color = 'grey'
+        item.isExcel = false
+    }
+}
+function cancelClick() {
+    drawer2.value = false
+    sql.value = ''
+    sqlBatch.value = []
+}
+function confirmClick() {
+    ElMessageBox.confirm(`Are you confirm to copy ?`)
+        .then(() => {
+            drawer2.value = false
+        })
+        .catch(() => {
+            // catch error
+        })
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.main {
+    height: 300px;
+    border: 0.5px rgb(209, 207, 207) solid;
+    border-radius: 5px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    padding: 0 5px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.main_input {
+    margin: 5px 0;
+    width: 48%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.main_input:first-child {
+    margin-right: 4%;
 }
-a {
-  color: #42b983;
+.ExcelXls{
+    display: inline-block;
+    margin: 0 10px;
 }
 </style>
